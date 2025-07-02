bd9e167d6d3fba6989e1136ace1ca3a75a28334e
lsg1024 <zks145@naver.com>
2025-07-03 00:14:42 +0900

    feat: Forwarded, UserAgent Filter 추가
    - 유저 ip 식별
    - 유저 device 식별

--- api-gateway/src/main/java/org/msa/filter/AuthorizationHeaderFilter.java
+++ api-gateway/src/main/java/org/msa/filter/AuthorizationHeaderFilter.java
@@ -23,7 +23,7 @@ public class AuthorizationHeaderFilter implements GlobalFilter, Ordered {
 
 //        String path = exchange.getRequest().getURI().getPath();
 //
-//        if (path.startsWith("/eureka") || path.startsWith("/actuator")) {
+//        if (path.startsWith("/eureka") || path.startsWith("/actuator") || path.startsWith("/auth/reissue") || path.startsWith("/auth/login")) {
 //            return chain.filter(exchange);
 //        }
 //
@@ -61,6 +61,6 @@ public class AuthorizationHeaderFilter implements GlobalFilter, Ordered {
 
     @Override
     public int getOrder() {
-        return -2;
+        return -4;
     }
 }
