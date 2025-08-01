6678d45b4ad347762a1d446c28c0c09d560bc404
lsg1024 <zks145@naver.com>
2025-08-01 18:59:11 +0900

    feat: 카프카 Retry 위한 Exception 추가

--- product-service/src/main/java/com/msa/product/global/exception/ExceptionMessage.java
+++ product-service/src/main/java/com/msa/product/global/exception/ExceptionMessage.java
@@ -4,4 +4,5 @@ public class ExceptionMessage {
     public static final String NOT_ACCESS = "권한이 부족합니다.\n관리자에게 문의하십시오.";
     public static final String NOT_FOUND = "대상을 찾을 수 없습니다.";
     public static final String IS_EXIST = "중복된 값입니다.";
+    public static final String BATCH_FAIL = "Batch 실패로 삭제 불가";
 }
