f74629243383aa4c8aa8901b45f49a921b0952fa
lsg1024 <zks145@naver.com>
2025-09-16 23:44:21 +0900

    refactor: StoneDto Global로 폴더 이동, 카프카 order, stock group 분리

--- order-service/src/main/java/com/msa/order/global/kafka/config/KafkaConsumerConfig.java
+++ order-service/src/main/java/com/msa/order/global/kafka/config/KafkaConsumerConfig.java
@@ -20,14 +20,38 @@ public class KafkaConsumerConfig {
 
     @Value("${kafka.uri}")
     private String KAFKA_BROKER_URL;
-    @Value("${kafka.group}")
-    private String KAFKA_GROUP_ID;
+    @Value("${kafka.order-group}")
+    private String KAFKA_ORDER_GROUP_ID;
+    @Value("${kafka.stock-group}")
+    private String KAFKA_STOCK_GROUP_ID;
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public ConsumerFactory<String, Object> orderConsumerFactory() {
         Map<String, Object> configs = new HashMap<>();
         configs.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER_URL);
-        configs.put(GROUP_ID_CONFIG, KAFKA_GROUP_ID);
+        configs.put(GROUP_ID_CONFIG, KAFKA_ORDER_GROUP_ID);
+        configs.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> orderKafkaListenerContainerFactory() {
+        final ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(orderConsumerFactory());
+        factory.getContainerProperties().setGroupId(KAFKA_ORDER_GROUP_ID);
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setMissingTopicsFatal(false);
+
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> stockConsumerFactory() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER_URL);
+        configs.put(GROUP_ID_CONFIG, KAFKA_STOCK_GROUP_ID);
         configs.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
@@ -37,7 +61,8 @@ public class KafkaConsumerConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
         final ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(stockConsumerFactory());
+        factory.getContainerProperties().setGroupId(KAFKA_STOCK_GROUP_ID);
         factory.setConcurrency(3);
         factory.getContainerProperties().setMissingTopicsFatal(false);
 
