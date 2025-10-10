452f8b3ddbeb6ea98a3408a05eed1e816e43c822
JS <zks145@naver.com>
2025-10-10 23:57:38 +0900

    feat: 재고 등록, 주문 등록 기능 작성
    - 주문 -> 재고 등록
    - 재고 -> 주문 등록

--- order-service/src/main/java/com/msa/order/local/stock/controller/StockController.java
+++ order-service/src/main/java/com/msa/order/local/stock/controller/StockController.java
@@ -11,6 +11,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class StockController {
 
@@ -53,14 +55,20 @@ public class StockController {
 
     @GetMapping("/stocks")
     public ResponseEntity<ApiResponse<CustomPage<StockDto.Response>>> getStocks(
-            @RequestParam(name = "search") String inputSearch,
-            @RequestParam(name = "startAt") String startAt,
-            @RequestParam(name = "endAt") String endAt,
-            @RequestParam(name = "type", required = false) String orderType,
+            @RequestParam(name = "search", required = false) String input,
+            @RequestParam(name = "start") String startAt,
+            @RequestParam(name = "end") String endAt,
+            @RequestParam(name = "factory", required = false) String factoryName,
+            @RequestParam(name = "store", required = false) String storeName,
+            @RequestParam(name = "setType", required = false) String setTypeName,
+            @RequestParam(name = "color", required = false) String colorName,
+            @RequestParam(name = "sortField", required = false) String sortField,
+            @RequestParam(name = "sortOrder", required = false) String sort,
+            @RequestParam(name = "order_status", required = false) String orderStatus,
             @PageableDefault(size = 20) Pageable pageable) {
 
-        StockDto.StockCondition condition = new StockDto.StockCondition(startAt, endAt);
-        CustomPage<StockDto.Response> stocks = stockService.getStocks(inputSearch, orderType, condition, pageable);
+        CustomPage<StockDto.Response> stocks = stockService.getStocks(input, startAt, endAt, factoryName,
+                storeName, setTypeName, colorName, sortField, sort, orderStatus, pageable);
 
         return ResponseEntity.ok(ApiResponse.success(stocks));
     }
@@ -104,5 +112,48 @@ public class StockController {
         stockService.recoveryStock(accessToken, flowCode, orderType);
         return ResponseEntity.ok(ApiResponse.success("복구 완료"));
     }
+
+    // 공장 리스트 배열 // 페이징 처리하면 로딩된 페이징 값의 데이터로만 구성 가능하니 별도 호출이 필요
+    @GetMapping("/stocks/filters/factory")
+    public ResponseEntity<ApiResponse<List<String>>> getFactoryNames(
+            @RequestParam(name = "start") String startAt,
+            @RequestParam(name = "end") String endAt,
+            @RequestParam(name = "order_status", required = false) String orderStatus) {
+
+        List<String> filterFactories = stockService.getFilterFactories(startAt, endAt, orderStatus);
+        return ResponseEntity.ok(ApiResponse.success(filterFactories));
+    }
+
+    // 상점 리스트 배열
+    @GetMapping("/stocks/filters/store")
+    public ResponseEntity<ApiResponse<List<String>>> getStoreNames(
+            @RequestParam(name = "start") String startAt,
+            @RequestParam(name = "end") String endAt,
+            @RequestParam(name = "order_status", required = false) String orderStatus) {
+
+        List<String> filterStores = stockService.getFilterStores(startAt, endAt, orderStatus);
+        return ResponseEntity.ok(ApiResponse.success(filterStores));
+    }
+
+    // 유형 리스트 배열
+    @GetMapping("/stocks/filters/set-type")
+    public ResponseEntity<ApiResponse<List<String>>> getSetTypeNames(
+            @RequestParam(name = "start") String startAt,
+            @RequestParam(name = "end") String endAt,
+            @RequestParam(name = "order_status", required = false) String orderStatus) {
+
+        List<String> filterSetType = stockService.getFilterSetType(startAt, endAt, orderStatus);
+        return ResponseEntity.ok(ApiResponse.success(filterSetType));
+    }
+
+    @GetMapping("/stocks/filters/color")
+    public ResponseEntity<ApiResponse<List<String>>> getColorNames(
+            @RequestParam(name = "start") String startAt,
+            @RequestParam(name = "end") String endAt,
+            @RequestParam(name = "order_status", required = false) String orderStatus) {
+        List<String> filterColors = stockService.getFilterColors(startAt, endAt, orderStatus);
+        return ResponseEntity.ok(ApiResponse.success(filterColors));
+    }
+
 
 }
