13679c450a1d5d2769d273ee32bed10197dc60b9
lsg1024 <zks145@naver.com>
2025-11-04 23:28:23 +0900

    style: 카프카 익셉션 위치 변경

--- common/src/main/java/com/msa/common/global/exception/KafkaProcessingException.java
+++ common/src/main/java/com/msa/common/global/exception/KafkaProcessingException.java
@@ -1,4 +1,4 @@
-package com.msa.order.global.exception;
+package com.msa.common.global.exception;
 
 public class KafkaProcessingException extends RuntimeException {
     public KafkaProcessingException(String message) {
