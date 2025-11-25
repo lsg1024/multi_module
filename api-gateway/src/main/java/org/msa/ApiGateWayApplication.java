c5b7a3b464a8809e7051dd3f5efe5819086f9386
lsg1024 <zks145@naver.com>
2025-11-25 16:22:35 +0900

    feat: CI/CD Api GateWay 테스트

--- api-gateway/src/main/java/org/msa/ApiGateWayApplication.java
+++ api-gateway/src/main/java/org/msa/ApiGateWayApplication.java
@@ -16,6 +16,7 @@ exclude = {
         FlywayAutoConfiguration.class
 })
 public class ApiGateWayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGateWayApplication.class, args);
     }
