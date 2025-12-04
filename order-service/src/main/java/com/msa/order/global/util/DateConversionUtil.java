f371377dcad281c6b95b480750c87e4b1f9c0183
lsg1024 <zks145@naver.com>
2025-12-04 23:52:16 +0900

    fix: 날짜 null 처리 오류 수정

--- order-service/src/main/java/com/msa/order/global/util/DateConversionUtil.java
+++ order-service/src/main/java/com/msa/order/global/util/DateConversionUtil.java
@@ -4,6 +4,7 @@ import org.springframework.util.StringUtils;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateConversionUtil {
 
@@ -15,10 +16,18 @@ public class DateConversionUtil {
             return null;
         }
 
-        LocalDate parse = LocalDate.parse(dateAt);
-        LocalTime now = LocalTime.now(KST);
-        LocalDateTime localDateTime = LocalDateTime.of(parse, now);
-        return localDateTime.atZone(KST).toOffsetDateTime();
+        if (dateAt.equalsIgnoreCase("null")) {
+            return null;
+        }
+
+        try {
+            LocalDate parse = LocalDate.parse(dateAt);
+            LocalTime now = LocalTime.now(KST);
+            LocalDateTime localDateTime = LocalDateTime.of(parse, now);
+            return localDateTime.atZone(KST).toOffsetDateTime();
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 
     public static OffsetDateTime LocalDateToOffsetDateTime(String dateAt) {
@@ -29,24 +38,6 @@ public class DateConversionUtil {
         LocalDateTime localDateTime = LocalDateTime.parse(dateAt, FORMATTER);
 
         return localDateTime.atZone(KST).toOffsetDateTime();
-    }
-
-    public static OffsetDateTime plusBusinessDay(OffsetDateTime base, Integer businessDays) {
-        if (businessDays < 0) {
-            throw new IllegalArgumentException("잘못된 날짜 설정입니다.");
-        }
-
-        OffsetDateTime d = base;
-        int added = 0;
-
-        while (added < businessDays) {
-            d = d.plusDays(1);
-            DayOfWeek w = d.atZoneSimilarLocal(KST).getDayOfWeek();
-            if (w != DayOfWeek.SATURDAY && w != DayOfWeek.SUNDAY) {
-                added++;
-            }
-        }
-        return d;
     }
 
 }
