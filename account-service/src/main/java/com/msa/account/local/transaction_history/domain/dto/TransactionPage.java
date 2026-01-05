9a08b116a738495edcdcb86384ba17865123bcb0
lsg1024 <zks145@naver.com>
2026-01-05 22:07:58 +0900

    refactor: 재질 컬럼 추가

--- account-service/src/main/java/com/msa/account/local/transaction_history/domain/dto/TransactionPage.java
+++ account-service/src/main/java/com/msa/account/local/transaction_history/domain/dto/TransactionPage.java
@@ -13,18 +13,20 @@ public class TransactionPage {
     private String accountName;
     private String accountHarry;
     private String createDate;
+    private String material;
     private String goldAmount;
     private String moneyAmount;
     private String tradeType;
     private String transactionNote;
 
     @QueryProjection
-    public TransactionPage(String saleCode, String accountId, String accountName, String accountHarry, String createDate, String goldAmount, String moneyAmount, SaleStatus tradeType, String transactionNote) {
+    public TransactionPage(String saleCode, String accountId, String accountName, String accountHarry, String createDate, String material, String goldAmount, String moneyAmount, SaleStatus tradeType, String transactionNote) {
         this.saleCode = saleCode;
         this.accountId = accountId;
         this.accountName = accountName;
-        this.accountHarry = accountHarry;
+        this.accountHarry = material.equals("24K") ? "1.00" : accountHarry;
         this.createDate = createDate.substring(0, 10);
+        this.material = material;
         this.goldAmount = goldAmount;
         this.moneyAmount = moneyAmount;
         this.tradeType = tradeType.getDisplayName();
